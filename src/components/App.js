42ec620a20040c5fbfc8e291f08acf047814e80a
Louis Klein <louisklein71@gmail.com>
2023-07-31 15:22:34 -0700

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import '../App.css';
+import React, {useState, useEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Header from "./Header";
+import Home from "./Home";
+import CommandList from "./CommandList";
+import Dashboard from "./Dashboard";
+import Server from "./Server";
+import BotStatus from "./BotStatus";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Home
-        </p>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/commands" element={<CommandList />} />
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/server" element={<Server />}/>
+        <Route path="/status" element={<BotStatus />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
